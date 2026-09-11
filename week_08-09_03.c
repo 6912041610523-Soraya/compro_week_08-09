57fb370f215666c1b965f7904a0de598ba7fae17
6912041610523-Soraya <s6912041610523@email.kmutnb.ac.th>
2026-09-11 14:58:37 +0700

    Refactor main function and adjust loop structure

--- week_08-09_03.c
+++ week_08-09_03.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     int i, j;
 
     for (i = 1; i <= 5; i ++) {
         for (j = 1; j <= i; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= 5 - i; j++) {
             printf("* ");
         }
 
         printf("\n");
+
     }
+        
 
     return 0;
 }
